a2e74cc719dbc17e9b90e36b91664a20ea3f55ac
Kayvan Mazaheri <kayvanmazaheri@gmail.com>
2017-11-04 14:28:19 +0330

    Add Homepage

--- DallalKosh/settings.py
+++ DallalKosh/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'DallalKosh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'DallalKosh/app/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
